1d96bb13d9020bc20ec52c442785f25e2550da1b
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2024-01-21 01:18:14 +0000

    d

--- src/components/Initial.js
+++ src/components/Initial.js
@@ -1,15 +1,34 @@
 import {Link} from 'react-scroll';
-// console.log("Função Initial(fora do componente)")
+import myImg from '../external/teste.jpg';
 
+let stack = ["JavaScript", "Bootstrap", "HTML", "React", "jQuery", "CSS", "Python", "Sass"]
 export default function Initial() {
   console.log("Componente Initial")
  
   
   return (
-    <div id="div1" className="content">. 
+    <div id="div1" className="content">
+      
+      <div id="topbar">
+        <h1><a href="#div1">Home</a></h1>
+        <h1><a href="#div2">myProjects</a></h1>
+        <h1><a href="#div3">About Me</a></h1>
+        <h1><a href="#div4">Contacts</a></h1>
+      </div>
+    
+      <div id="central">
+        <img src={myImg} alt="My Image" id="my_img"/>
+
+        <div id="mystack">
+          <h1>I work with: </h1>
+          <p>{stack}</p>
+        </div>
+
+      </div>
 
     <Link to="3">Clicking will go to ID=3</Link>
 
     </div>
   );
-} 
+}
+
