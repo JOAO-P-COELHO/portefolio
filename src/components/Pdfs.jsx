27afb145fd82b434412dd1e23ca457916d335587
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2024-01-27 23:25:48 +0000

    .

--- src/components/Pdfs.jsx
+++ src/components/Pdfs.jsx
@@ -3,18 +3,16 @@ import { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 
 
-
 export default function Pdfs(props) {
   const [numPages, setNumPages] = useState();
   const [pageNumber, setPageNumber] = useState(1);
   let file = props.file
-  console.log(file)
 
  
   function onDocumentLoadSuccess({ numPages }){
     setNumPages(numPages);
   }
-
+// The required above it's extremely important. Just file="props.file" doesn't work, because he doesn't know what to do with the string, even if I have the imports at the beginning of the .jsx file. A require it's needed in this case, so that React can import the file based on the props (the location to find it depends on it)
   return (
     <div className='pdfs' >
       <Document file={require(`../external/projs/pdfs/${props.file}.pdf`)}   onLoadSuccess={onDocumentLoadSuccess}>
